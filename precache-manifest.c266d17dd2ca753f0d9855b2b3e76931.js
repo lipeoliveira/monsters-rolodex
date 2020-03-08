ffa1a330b86f9f3c33c7a1cafa91f93fdb7e04a6
lipeoliveira <fe-li-pe_2007@hotmail.com>
2020-03-08 16:44:36 -0300

    Updates

---
+++ precache-manifest.c266d17dd2ca753f0d9855b2b3e76931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e5312519969dec177ab7a822b46e75",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7e82bb0b09c54eeeb8cb",
+    "url": "/monsters-rolodex/static/css/main.9bffa294.chunk.css"
+  },
+  {
+    "revision": "bda571518dbe16be5b25",
+    "url": "/monsters-rolodex/static/js/2.44b2c4fe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.44b2c4fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e82bb0b09c54eeeb8cb",
+    "url": "/monsters-rolodex/static/js/main.2423d229.chunk.js"
+  },
+  {
+    "revision": "a52390781e72ed913537",
+    "url": "/monsters-rolodex/static/js/runtime-main.4eb16b66.js"
+  }
+]);
